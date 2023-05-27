bcd7b70b51f393c3a9b4a83b3467e56521d1ef0b
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-26 21:47:00 -0700

    all routes working, had req res reversed at first.

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => { res.json("Here are the notes (but not really)") });
+router.get('/', (req, res) => { res.json("GET all notes (but not really)"); });
+
+router.get('/:id', (req, res) => { res.json("GET a single note"); });
+
+
+router.post('/', (req, res) => { res.json("POST a single note"); });
+
+router.delete('/:id', (req, res) => { res.json("DELETE a single note"); });
+
+router.patch('/:id', (req, res) => { res.json("POST a note edit"); });
 
 module.exports = router;
