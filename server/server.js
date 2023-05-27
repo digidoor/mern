f6a7e5f34195ed9119fec415e0953e95bba2060c
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-26 21:12:09 -0700

    added middleware that simply logs reqs to console

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const express = require('express');
 
 //express app
 const app = express();
+app.use( (req, res, next) =>
+{
+    console.log(req.path, req.method);
+    next();
+});
 
 //router init
 app.get('/', (req, res) => { res.json("Welcome to the test app.") })
