85f69fe26b34c06095bf89adfb5fabe24819923c
digidoor <37601466+digidoor@users.noreply.github.com>
2023-05-26 21:22:05 -0700

    separate routes folder

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const noteRoutes = require('./routes/notes');
 
 //express app
 const app = express();
@@ -10,10 +11,9 @@ app.use( (req, res, next) =>
 });
 
 //router init
-app.get('/', (req, res) => { res.json("Welcome to the test app.") })
+//app.get('/', (req, res) => { res.json("Welcome to the test app.") });
+
+app.use(noteRoutes);
 
 //listen for requests
-app.listen(process.env.PORT, () =>
-{
-    console.log(`listening on port ${process.env.PORT}!!!`);
-});
+app.listen(process.env.PORT, () => { console.log(`listening on port ${process.env.PORT}!!!`); });
